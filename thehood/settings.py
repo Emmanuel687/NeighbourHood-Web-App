46181a647c95b8b87c3e0734f4e2d69d5dfeae34
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-17 16:55:28 +0300

    Dev:Created a database neighbourhood

--- thehood/settings.py
+++ thehood/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'thehood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
+        'USER': 'manu',
+    'PASSWORD': '39906875'
     }
 }
 
