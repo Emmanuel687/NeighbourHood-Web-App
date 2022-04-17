089e93290e2cf643177b294c51998339ccc1318f
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-17 18:40:42 +0300

    Dev:Created a Media

--- thehood/settings.py
+++ thehood/settings.py
@@ -22,7 +22,7 @@ cloudinary.config(
    api_key = "796298925861151", 
    api_secret = "gCImM06WuKyPbZ-hRopEt13VuK4" 
  )
- 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +141,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
