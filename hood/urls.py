451e546a0f78ba985e37c9bb2ef4b4d39966f7b6
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-17 18:21:05 +0300

    Dev:Created template urls

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,22 @@
-from django.conf.urls import re_path
+from django.conf.urls import re_path,path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib import admin
 
 urlpatterns=[
-    re_path('^$',views.welcome,name = 'welcome'),
+    # re_path('^$',views.welcome,name = 'welcome'),
+    path('', views.home, name = 'home'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
+    path('profile/', views.profile, name='profile'),
+    path('hood/', views.hoods, name='hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('new-hood/', views.new_hood, name='new-hood'),
+    path('<hood_id>/news', views.news, name='news'),                    
+    path('<hood_id>/business',views.business,name='business'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('businesses/<hood_id>', views.businesses, name='businesses'),
+    path('post/<hood_id>', views.post, name='post'),
+    path('search/',views.search_results,name='search_results'),
+
 ]
